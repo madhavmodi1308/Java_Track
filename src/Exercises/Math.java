38a247117d933c790a8b2c5a11dfc9fac8c68a37
madhavmodi1308 <madhavmodi1308@gmail.com>
2021-03-26 12:24:49 +0530

    added Switch Case

--- src/Exercises/Math.java
+++ src/Exercises/Math.java
@@ -9,7 +9,7 @@ public class Math {
 		this.a = a;
 		this.b = b;
 	}
-	
+
 	public int getA() {
 		return a;
 	}
@@ -44,6 +44,19 @@ public class Math {
 
 	public float conditionalOperation() {
 
+//		switch((a<b)?1:(a>b && b!=0)?2:(a==b)?3:(a>b )?4:-1){
+//		case 1:
+//			return sum();
+//		case 2:
+//			return div();
+//		case 3:
+//			return mul();
+//		case 4:
+//			return subtract();
+//		default:
+//			System.out.println("Invalid Condition");
+//		}
+
 		if(a<b) {
 			return sum();
 		}
@@ -59,18 +72,20 @@ public class Math {
 
 		return 0;
 	}
-	
+
 	public int func(int a, int b) {
 		if(a>b) return -1;
 		int sum = 0;
+
 //		for(int i = a; i<=b; i++) {
 //			sum+=i;
 //		}
+
 		while(a<=b) {
 			sum+=a;
 			a++;
 		}
-		
+
 		return sum;
 	}
 
